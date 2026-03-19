81e1c9bbc454555cbad27dfc933c7955d55edd68
atentadosam-lgtm <atentadosam@gmail.com>
2026-03-19 10:50:32 -0300

    Refactor data logging format in savar-dados.php

--- savar-dados.php
+++ savar-dados.php
@@ -1,22 +1,10 @@
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-
 $data = json_decode(file_get_contents('php://input'), true);
 
 if ($data) {
-    $line = sprintf(
-        "%s | %s | %s | %s\n",
-        date('Y-m-d H:i:s'),
-        $data['nome'],
-        $data['telefone'],
-        $data['cpf']
-    );
-    
+    $line = "{$data['data']} | {$data['nome']} | {$data['telefone']} | {$data['cpf']}\n";
     file_put_contents('solicitacoes.txt', $line, FILE_APPEND | LOCK_EX);
-    
     echo json_encode(['success' => true]);
-} else {
-    echo json_encode(['success' => false]);
 }
 ?>
